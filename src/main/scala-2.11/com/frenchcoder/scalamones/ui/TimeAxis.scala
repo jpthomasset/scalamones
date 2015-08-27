dab2f157a8e04b38b9d4ac25b409446bacddbec1
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2015-08-27 18:11:24 +0200

    Cleanup TimeAxis

--- src/main/scala-2.11/com/frenchcoder/scalamones/ui/TimeAxis.scala
+++ src/main/scala-2.11/com/frenchcoder/scalamones/ui/TimeAxis.scala
@@ -2,8 +2,7 @@ package com.frenchcoder.scalamones.ui
 
 import java.text.SimpleDateFormat
 import java.util.Date
-import javafx.scene.chart
-import javafx.scene.chart.{NumberAxis, ValueAxis}
+import javafx.scene.chart.ValueAxis
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 
@@ -11,39 +10,18 @@ import scala.collection.JavaConverters._
  *
  */
 class TimeAxis extends ValueAxis[java.lang.Long] {
-  override def calculateMinorTickMarks(): java.util.List[java.lang.Long] = {
-    if(currentRange != null) {
-      val ticks = (for(n <- currentRange.min until(currentRange.max, currentRange.minorTickStep))
-        yield n.toLong.asInstanceOf[java.lang.Long]
-        )
-
-      println("MinorTicks " +  ticks.size)
-      ticks.asJava
-    } else {
-      List.empty[java.lang.Long]
-    }
-  }
-
   case class Range(scale:Double, min: Double, max: Double, majorTickStep: Double, minorTickStep: Double)
 
-  var currentRange:Range = autoRange(1440655187000.0, 1440655387000.0, 200, 20).asInstanceOf[Range]
-  setRange(currentRange, false)
+  var currentRange:Range = null
+  setRange(autoRange(1, 100, 200, 20).asInstanceOf[Range], false)
 
-  override def getTickMarkLabel(t: java.lang.Long): String = {
-    val s = (new SimpleDateFormat("hh:mm:ss")).format(new Date(t))
-    println(s"Formating ${t} => ${s}")
-    s
-  }
+  override def getTickMarkLabel(t: java.lang.Long): String = new SimpleDateFormat("kk:mm:ss").format(new Date(t))
 
-  override def getRange = {
-    //println("Get Range")
-    currentRange
-  }
+  override def getRange = currentRange
 
   override def setRange(o: scala.Any, b: Boolean): Unit = {
     if(o != null) {
       currentRange = o.asInstanceOf[Range]
-      println("setRange with value " + currentRange)
       setLowerBound(currentRange.min)
       currentLowerBound.set(currentRange.min)
       setUpperBound(currentRange.max)
@@ -52,26 +30,26 @@ class TimeAxis extends ValueAxis[java.lang.Long] {
   }
 
   override def calculateTickValues(v: Double, o: scala.Any): java.util.List[java.lang.Long] = {
-    println("calculateTickValues")
     if(o != null) {
       val r = o.asInstanceOf[Range]
-      println(s"calculateTickValues ${r.min} -> ${r.max}")
-
       val seq = if(r.min == r.max) Seq(r.min) else r.min until(r.max, r.majorTickStep)
 
-      val ticks = (for(n <- seq)
-        yield n.toLong.asInstanceOf[java.lang.Long]
-      )
+      (for(n <- seq) yield n.toLong.asInstanceOf[java.lang.Long]).asJava
+    } else {
+      List.empty[java.lang.Long]
+    }
+  }
 
-      println("Ticks " +  ticks.size)
-      ticks.asJava
+  override def calculateMinorTickMarks(): java.util.List[java.lang.Long] = {
+    if(currentRange != null) {
+      (for(n <- currentRange.min until(currentRange.max, currentRange.minorTickStep))
+        yield n.toLong.asInstanceOf[java.lang.Long]).asJava
     } else {
       List.empty[java.lang.Long]
     }
   }
 
   override def autoRange(minValue: Double, maxValue: Double, length: Double, labelSize: Double) : AnyRef = {
-    println(s"autorange(${minValue}, ${maxValue}, ${length}, ${labelSize})")
 
     val range = maxValue - minValue
     val paddedRange = if(range == 0) 2 else range*1.02
@@ -79,9 +57,10 @@ class TimeAxis extends ValueAxis[java.lang.Long] {
     val paddedMin = minValue - padding
     val paddedMax = maxValue + padding
 
-    val majorStep = (paddedMax - paddedMin) / (length / labelSize)
-
-    Range(calculateNewScale(length, paddedMin, paddedMax), paddedMin, paddedMax, majorStep, majorStep/2)
+    val majorStep = (paddedMax - paddedMin) / (length / (4*labelSize))
+    println(s"length ${length} - ${labelSize}")
+    println(s"Step ${(paddedMax - paddedMin)} / ${(length / (4*labelSize))} => ${majorStep}")
+    Range(calculateNewScale(length, paddedMin, paddedMax), paddedMin, paddedMax, majorStep, majorStep / 2)
 
   }
 }
